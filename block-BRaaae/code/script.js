8754b92d9565913c52b66a682664577e5caa9220
Sumanta Kabiraj <sumantablog@gmail.com>
2022-09-07 12:40:32 +0530

    Assignment: Refactor Movie App Complete

---
+++ block-BRaaae/code/script.js
@@ -0,0 +1,84 @@
+let input = document.querySelector("input");
+var container = document.querySelector("ul");
+var root = ReactDOM.createRoot(container);
+function handleToggle(event) {
+  let id = event.target.dataset.id;
+  let moviesList = Array.from(JSON.parse(localStorage.getItem("moviesList")));
+  moviesList[id].isWatched = !moviesList[id].isWatched;
+  localStorage.setItem("moviesList", JSON.stringify(moviesList));
+  displayMovies();
+}
+
+function handleDelete(event) {
+  let moviesList = Array.from(JSON.parse(localStorage.getItem("moviesList")));
+  moviesList.splice(event.target.dataset.id, 1);
+  localStorage.setItem("moviesList", JSON.stringify(moviesList));
+  displayMovies();
+}
+
+let createElm = React.createElement;
+
+function displayMovies() {
+  if (localStorage.getItem("moviesList") == null) return;
+  let moviesArr = Array.from(JSON.parse(localStorage.getItem("moviesList")));
+  let list = moviesArr.map((elm, index) => {
+    return createElm(
+      "li",
+      {
+        key: index,
+        className: "flex-1",
+      },
+
+      createElm(
+        "p",
+        {
+          className: "name",
+        },
+        elm.name
+      ),
+
+      createElm(
+        "p",
+        {},
+
+        createElm(
+          "span",
+          {
+            "data-id": index,
+            onClick: handleToggle,
+          },
+          elm.isWatched ? "Watched" : "To Watched"
+        ),
+
+        createElm(
+          "span",
+          {
+            "data-id": index,
+            onClick: handleDelete,
+          },
+          "Remove"
+        )
+      )
+    );
+  });
+
+  root.render(list);
+}
+
+// Handling Input
+input.addEventListener("keyup", (event) => {
+  if (event.keyCode === 13 && event.target.value) {
+    localStorage.setItem(
+      "moviesList",
+      JSON.stringify([
+        ...JSON.parse(localStorage.getItem("moviesList") || "[]"),
+        { name: event.target.value, isWatched: false },
+      ])
+    );
+
+    event.target.value = "";
+    displayMovies();
+  }
+});
+
+displayMovies();
